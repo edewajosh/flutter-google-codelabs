096b286c4842efbbccf70eea8268529b5351f3c6
edewajosh <joshedewa@gmail.com>
2021-05-13 14:43:44 +0300

    looking nice

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator...',
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
       home: RandonWords(),
     );
   }
@@ -20,7 +23,9 @@ class RandonWords extends StatefulWidget {
 
 class _RandonWordsState extends State<RandonWords> {
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
+
   @override
   Widget build(BuildContext context) {
     // final wordPair = WordPair.random();
@@ -28,21 +33,23 @@ class _RandonWordsState extends State<RandonWords> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator...'),
+        actions: [IconButton(icon: Icon(Icons.list), onPressed: _pushSaved)],
+        backgroundColor: Colors.blueAccent,
       ),
       body: _buildSuggestions(),
     );
   }
 
-  Widget _buildSuggestions(){
+  Widget _buildSuggestions() {
     return ListView.builder(
       padding: const EdgeInsets.all(16),
-      // The itemBuilder callback is called 
-      itemBuilder: (BuildContext _context, int i){
-        if (i.isOdd){
+      // The itemBuilder callback is called
+      itemBuilder: (BuildContext _context, int i) {
+        if (i.isOdd) {
           return Divider();
         }
         final int index = i ~/ 2;
-        if (index >= _suggestions.length){
+        if (index >= _suggestions.length) {
           _suggestions.addAll(generateWordPairs().take(10));
         }
         return _buildRow(_suggestions[index]);
@@ -51,11 +58,54 @@ class _RandonWordsState extends State<RandonWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context)
+        .push(MaterialPageRoute<void>(builder: (BuildContext context) {
+      final tiles = _saved.map(
+        (WordPair pair) {
+          return ListTile(
+            title: Text(
+              pair.asPascalCase,
+              style: _biggerFont,
+            ),
+          );
+        },
+      );
+      final divided = tiles.isNotEmpty
+          ? ListTile.divideTiles(context: context, tiles: tiles).toList()
+          : <Widget>[];
+
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Saved suggestions'),
+          actions: [],
+        ),
+        body: ListView(
+          children: divided,
+        ),
+      );
+    }));
   }
 }
