84ddd59e8315c936330279487ba42607c3bb3670
edewajosh <joshedewa@gmail.com>
2021-05-13 11:15:48 +0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: RandonWords(),
+        ),
+      ),
+    );
+  }
+}
+
+class RandonWords extends StatefulWidget {
+  @override
+  _RandonWordsState createState() => _RandonWordsState();
+}
+
+class _RandonWordsState extends State<RandonWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
